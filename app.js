72b83694945464388a5310ed5d8e0a0a6f907581
conelo03 <ramadhan071thea@gmail.com>
2022-08-17 02:51:21 +0700

    crud

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const mysql = require('mysql');
-
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -11,16 +12,63 @@ const connection = mysql.createConnection({
   database: 'db_latihan_node_js_1'
 });
 
-connection.connect(function(error){
-  if(!!error){
-    console.log(error);
-  }else{
-    console.log('Completly Connected!');
-  }
-})
-
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      res.render('index.ejs', {items: results});
+    }
+  );
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO items (name) VALUES (?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+app.post('/delete/:id', (req, res) => {
+  connection.query(
+    'DELETE FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+app.get('/edit/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.render('edit.ejs', {item: results[0]});
+    }
+  );
+});
+
+app.post('/update/:id', (req, res) => {
+  // Ketik code untuk memperbarui item yang dipilih
+  connection.query(
+    'UPDATE items set name= ? WHERE id = ?',
+    [req.body.itemName, req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+  // Hapus code dibawah ini yang redirect ke halaman Daftar Belanjaan
 });
 
 app.listen(3000);
